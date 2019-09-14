cb7954a9c34075d7f5abd434197ed468d5c40e9c
acgzr <48869975+acgzr@users.noreply.github.com>
2019-09-14 18:57:32 -0400

    M/G/k/FIFO/k/∞ Model v2.2
    v2.2: Code optimized to be shorter and faster. (Oldest version available)
    
    This model is also known as M/G/c/FCFS/c/∞, M/G/s/FCFS/s/∞ or Blocked Clients Cleared (BCC) system.
    - Arrivals rate fits a Poisson distribution
    - Service time fits a general or arbitrary distribution with standard deviation σ (sigma)
    - k/c/s is both the number of servers/channels and the system client capacity, so a client can enter the system only if there is a server available
    - The queue is non-existent (Lq=Wq=Pw=0) but the system has client losses instead
    - First-In-First-Out or First-Come-First-Served queuing discipline
    - The total client population is considered infinite

---
+++ mgkk.py
@@ -0,0 +1,26 @@
+import array
+A = float(input('Enter Lambda = '))
+U = float(input('Enter Mu = '))
+k = int(input('Enter k = '))
+Cs = float(input('Enter Cs (0?) = '))
+sum1 = 1; iF = 1
+for i in range(1,k+1):
+    iF *= i
+    sum1 += ((A/U)**i)/iF
+P = array.array('f', [1/sum1])
+jF = 1
+for j in range(1,k+1):
+    jF *= j
+    P.append((((A/U)**j)/jF)/sum1)
+for j in range(k+1):
+    print("P({0:d}) = {1:.4G}%".format(j,P[j]*100))
+L = (1-P[k])*A/U
+print("L = {0:.4G}".format(L))
+W = L/A
+print("W = {0:.4G}".format(W))
+Rho = A/(k*U)
+print("Rho (p) = {0:.4G}".format(Rho))
+if Cs != 0:
+    CT = Cs*k
+    print("CT = Cs*k = {0:.2f}*{1:.4G} =".format(Cs,k))
+    print("   = {0:.2f}".format(Cs*k))
