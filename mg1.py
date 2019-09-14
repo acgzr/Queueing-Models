a4b71871bb6748e30c6127f08afa888720e52313
acgzr <48869975+acgzr@users.noreply.github.com>
2019-09-14 18:44:13 -0400

    M/G/1/FIFO/∞/∞ Model v2.2
    v2.2: Code optimized to be shorter and faster. (Oldest version available)
    
    This model is also known as M/G/1/FCFS/∞/∞ or M/G/1.
    - Arrivals rate fits a Poisson distribution
    - Service time fits a general or arbitrary distribution with standard deviation σ (sigma)
    - Only one server/channel
    - First-In-First-Out or First-Come-First-Served queuing discipline
    - Both the system capacity and the total client population are considered infinite

---
+++ mg1.py
@@ -0,0 +1,24 @@
+import array
+A = float(input('Enter Lambda = '))
+U = float(input('Enter Mu = '))
+D = float(input('Enter Sigma (0?) = '))
+Cw = float(input('Enter Cw (0?) = '))
+Cs = float(input('Enter Cs (0?) = '))
+P = array.array('f', [1-A/U])
+print("P(0) = {0:.4G}%".format(P[0]*100))
+P.append(A/U)
+print("P(1) = Pw = Rho (p) = {0:.4G}%".format(P[1]*100))
+Lq = (A**2*D**2+(A/U)**2)/(2*(1-A/U))
+print("Lq = {0:.4G}".format(Lq))
+L = Lq+A/U
+print("L = {0:.4G}".format(L))
+Wq = Lq/A
+print("Wq = {0:.4G}".format(Wq))
+W = Wq+1/U
+print("W = {0:.4G}".format(W))
+if Cw != 0 or Cs != 0:
+    CT = Cw*L+Cs
+    print("CT = Cw*L + Cs =")
+    print("   = {0:.2f}*{1:.4G} + {2:.2f} =".format(Cw,L,Cs))
+    print("   = {0:.2f} + {1:.2f} =".format(Cw*L,Cs))
+    print("   = {0:.2f}".format(CT))
