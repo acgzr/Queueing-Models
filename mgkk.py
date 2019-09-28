14b6c67982ef8de65a7d4b54b857dc5910164c81
acgzr <48869975+acgzr@users.noreply.github.com>
2019-09-28 19:58:19 -0400

    M/G/k/FIFO/k/∞ Model v2.4
    v2.4: Code optimized to be shorter and faster by using lists instead of arrays. There was also a minor correction in the code.
    v2.3: You can now input expressions instead of numbers only.
    v2.2: Code optimized to be shorter and faster. (Oldest version available)
    
    This model is also known as M/G/c/FCFS/c/∞, M/G/s/FCFS/s/∞ or Blocked Clients Cleared (BCC) system.
    - Arrivals rate fits a Poisson distribution
    - Service time fits a general or arbitrary distribution with standard deviation σ (sigma)
    - k/c/s is both the number of servers/channels and the system client capacity, so a client can enter the system only if there is a server available
    - The queue is non-existent (Lq=Wq=Pw=0) but the system has client losses instead
    - First-In-First-Out or First-Come-First-Served queuing discipline
    - The total client population is considered infinite

--- mgkk.py
+++ mgkk.py
@@ -1,18 +1,17 @@
-import array
 A = float(eval(input('Enter Lambda = ')))
 U = float(eval(input('Enter Mu = ')))
 k = int(eval(input('Enter k = ')))
 Cs = float(eval(input('Enter Cs (0?) = ')))
-sum1 = 1; iF = 1
+sum1 = iF = 1
 for i in range(1,k+1):
     iF *= i
     sum1 += ((A/U)**i)/iF
-P = array.array('f', [1/sum1])
+P = [1/sum1]
+print("P({0:d}) = {1:.4G}%".format(j,P[0]*100))
 jF = 1
 for j in range(1,k+1):
     jF *= j
-    P.append((((A/U)**j)/jF)/sum1)
-for j in range(k+1):
+    P += [(((A/U)**j)/jF)/sum1]
     print("P({0:d}) = {1:.4G}%".format(j,P[j]*100))
 L = (1-P[k])*A/U
 print("L = {0:.4G}".format(L))
@@ -22,6 +21,5 @@ Rho = A/(k*U)
 print("Rho (p) = {0:.4G}".format(Rho))
 if Cs != 0:
     CT = Cs*k
-    print("CT = Cs*k = {0:.2f}*{1:.4G} =".format(Cs,k))
-    print("   = {0:.2f}".format(Cs*k))
-#190920
+    print("CT = Cs*k = {0:.2f}".format(Cs*k))
+#190928
