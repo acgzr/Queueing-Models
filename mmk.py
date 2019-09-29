66023c9dea365d4fb662b489fc6357408a7df48e
acgzr <48869975+acgzr@users.noreply.github.com>
2019-09-29 00:16:48 -0400

    M/M/k/FIFO/∞/∞ Model v3.0
    v3.0: Heavy optimization.
    v2.4: Code optimized to be shorter and faster by using lists instead of arrays. There were also minor corrections in the code.
    v2.3: You can now input expressions instead of numbers only.
    v2.2: Minor corrections.
    v2.1: Updated description.
    v2: Code optimized to be shorter and faster. (Oldest version available)
    
    This model is also known as M/M/c/FCFS/∞/∞, M/M/s/FCFS/∞/∞, M/M/k, M/M/c or M/M/s.
    - Arrivals rate fits a Poisson distribution
    - Service time fits an exponential distribution
    - k/c/s is the number of servers/channels
    - First-In-First-Out or First-Come-First-Served queuing discipline
    - Both the system capacity and the total client population are considered infinite

--- mmk.py
+++ mmk.py
@@ -5,25 +5,20 @@ kmax = int(eval(input('Enter kmax (kmin?) = ')))
 Pnmax = int(eval(input('Enter Pnmax (0?) = ')))
 Cw = float(eval(input('Enter Cw (0?) = ')))
 Cs = float(eval(input('Enter Cs (0?) = ')))
+F = []; F = [1]+[F[-1] for n in range(1, kmax+1) if not F.append(F[-1]*n if F else 1)]
 for k in range(int(1+A//U), kmax+1):
     print()
-    print("k = {0:d} ################".format(k))
-    sum1 = nf = 1
-    for n in range(1, k):
-        nf *= n
-        sum1 += ((A/U)**n)/nf
-    kf = nf * k
-    P = [1/(sum1+((A/U)**k)*(k*U)/(kf*(k*U-A)))]
-    print("P({0:d}) = {1:.4G}%".format(0,P[0]*100))
-    nf = 1
+    print("k = {0:d}".format(k))
+    sum1 = []; sum1 = [sum1[-1] for n in range(k) if not sum1.append(sum1[-1]+(A/U)**n/F[n] if sum1 else 1)]
+    P = [1/(sum1[-1]+(A/U)**k*(k*U)/(F[k]*(k*U-A)))]
+    print("P({0:d}) = {1:.4G}%".format(0, P[0]*100))
     for n in range(1, k+1):
-        nf *= n
-        P += [P[0]*((A/U)**n)/nf]
-        print("P({0:d}) = {1:.4G}%".format(n,P[n]*100))
+        P += [P[0]*(A/U)**n/F[n]]
+        print("P({0:d}) = {1:.4G}%".format(n, P[n]*100))
     for n in range(k+1, Pnmax+1):
-        P += [P[0]*((A/U)**n)/(kf*k**(n-k))]
-        print("P({0:d}) = {1:.4G}%".format(n,P[n]*100))
-    Lq = P[0]*(((A/U)**k)*A*U)/((k*U-A)**2*kf/k)
+        P += [P[0]*(A/U)**n/(F[k]*k**(n-k))]
+        print("P({0:d}) = {1:.4G}%".format(n, P[n]*100))
+    Lq = P[0]*(A/U)**k*A*U/((k*U-A)**2*F[k]/k)
     print("Lq = {0:.4G}".format(Lq))
     L = Lq+A/U
     print("L = {0:.4G}".format(L))
@@ -33,11 +28,11 @@ for k in range(int(1+A//U), kmax+1):
     print("W = {0:.4G}".format(W))
     Rho = A/(k*U)
     print("Rho (p) = {0:.4G}".format(Rho))
-    Pw = P[0]*k*U*A**k/(kf*(k*U-A)*U**k)
+    Pw = P[0]*k*U*A**k/(F[k]*(k*U-A)*U**k)
     print("Pw = {0:.4G}%".format(Pw*100))
     if Cw != 0 or Cs != 0:
         CT = Cw*L+Cs*k
         print("CT = Cw*L + Cs*k =")
-        print("   = {0:.2f} + {1:.2f} =".format(Cw*L,Cs*k))
+        print("   = {0:.2f} + {1:.2f} =".format(Cw*L, Cs*k))
         print("   = {0:.2f}".format(CT))
-#190928
+#190929
